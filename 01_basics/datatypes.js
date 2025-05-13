e9f2b35e9841edc2eed7e00c6bfeac917f8dba71
Achyuth69 <achuthparisha005@gmail.com>
2025-05-13 07:48:55 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("achyuth")
+
+let name="Achyuth"
+let age = 20
+let isLoggedIn = false
+
+/*
+number =>2^53
+bigint
+string=>""
+boolean = true/false
+null=> stand alone value
+undefined=>
+symbol -> uniqueness
+
+
+
+object
+
+*/
+
+console.log(typeof"achyuth")
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ 01_basics/var.js
@@ -0,0 +1,14 @@
+const accountId = 477705643
+let accountEmail = "achuthparisha005@gmail.com"
+var accountPassword = "sai3827"
+accountCity = "Hyderabad"
+
+// accountId = 2
+
+accountEmail = "sai@gmail.com"
+accountPassword="1234567890"
+accountCity="telangana"
+
+//console.log(accountId);
+
+console.table({accountId,accountEmail,accountPassword,accountCity});
