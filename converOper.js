c7d665735dc743af86635af957785932b4800b7d
Achyuth69 <achuthparisha005@gmail.com>
2025-05-13 08:13:25 +0000

    converOper

---
+++ converOper.js
@@ -0,0 +1,23 @@
+let score = "99"
+//true(1) //undefined(NaN) //null(0) //"100"(100)
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+
+let isLoggedIn="achyuth"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 35
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
